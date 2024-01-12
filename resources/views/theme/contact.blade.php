68678eab1a396efb18e19e9192062396e6194e9e
Shourov Sarker <80618479+Shourov-Sarker2021@users.noreply.github.com>
2024-01-12 15:51:21 +0600

    Update
    Done

--- resources/views/theme/contact.blade.php
+++ resources/views/theme/contact.blade.php
@@ -84,14 +84,14 @@
  
 
     <!-- Js Plugins -->
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.magnific-popup.min.js"></script>
-    <script src="js/jquery.slicknav.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/jquery.nice-select.min.js"></script>
-    <script src="js/mixitup.min.js"></script>
-    <script src="js/main.js"></script>
+    <script src="{{asset('theme/js/jquery-3.3.1.min.js')}}"></script>
+    <script src="{{asset('theme/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('theme/js/jquery.magnific-popup.min.js')}}"></script>
+    <script src="{{asset('theme/js/jquery.slicknav.js')}}"></script>
+    <script src="{{asset('theme/js/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('theme/js/jquery.nice-select.min.js')}}"></script>
+    <script src="{{asset('theme/js/mixitup.min.js')}}"></script>
+    <script src="{{asset('theme/js/main.js')}}"></script>
 </body>
 
 </html>
